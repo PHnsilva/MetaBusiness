e47bd74d93cac534090b8df44c3f06a661372929
phnsilva <phnsilva1@gmail.com>
2026-01-30 21:13:17 -0300

    fix: dependencias pom

---
+++ src/main/java/com/example/MetaBusiness/MetaBusinessApplication.java
@@ -0,0 +1,12 @@
+package com.example.MetaBusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaBusinessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetaBusinessApplication.class, args);
+    }
+}
